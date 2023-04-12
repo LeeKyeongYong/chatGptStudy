1224d0c0d0f37155d85b42f0f914296ea7483b29
이경용 <sleekydz86@naver.com>
2023-04-12 12:30:51 +0900

    소스정리
    최종 반영하는데 있어서 왜 오류난지 몰라  
    삽질함...

--- src/main/java/com/example/chatgpttutorial/config/ChatConfig.java
+++ src/main/java/com/example/chatgpttutorial/config/ChatConfig.java
@@ -11,19 +11,3 @@ public class ChatConfig {
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
     public static final String URL = "https://api.openai.com/v1/completions";
 }
-
-
-/*
-
-public class ChatGptConfig {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "Your-API-Key";
-    public static final String MODEL = "text-davinci-003";
-    public static final Integer MAX_TOKEN = 300;
-    public static final Double TEMPERATURE = 0.0;
-    public static final Double TOP_P = 1.0;
-    public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
-    public static final String URL = "https://api.openai.com/v1/completions";
-}
- */
